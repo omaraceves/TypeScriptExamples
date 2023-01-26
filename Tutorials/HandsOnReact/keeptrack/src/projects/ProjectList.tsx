b5e98ee22e89bfec48d809a31583a97122a50f91
Omar.Aceves <lcc.omar.aceves@gmail.com>
2023-01-25 22:31:15 -0800

    Implemented Lab 6

--- Tutorials/HandsOnReact/keeptrack/src/projects/ProjectList.tsx
+++ Tutorials/HandsOnReact/keeptrack/src/projects/ProjectList.tsx
@@ -6,7 +6,24 @@ interface ProjectListProps {
 }
 
 function ProjectList({ projects }: ProjectListProps) {
-    return <pre>{JSON.stringify(projects, null, ' ')}</pre>;
+    return (
+        <div className="row">
+            {projects.map((x) => (
+                <div key={x.id} className="cols-sm">
+                     <div className="card">
+                        <img src={x.imageUrl} alt={x.name} />
+                        <section className="section dark">
+                            <h5 className="strong">
+                                <strong>{x.name}</strong>
+                            </h5>
+                            <p>{x.description}</p>
+                            <p>Budget : {x.budget.toLocaleString()}</p>
+                        </section>
+                     </div>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default ProjectList;
