2acc3d24b3d3828eef538d7cd7a9ea95991f8873
Omar.Aceves <lcc.omar.aceves@gmail.com>
2023-02-16 15:26:52 -0800

    handsonreact lab 18

---
+++ Tutorials/HandsOnReact/keeptrack/src/projects/projectApi.ts
@@ -0,0 +1,47 @@
+import { Project } from './Project';
+const baseUrl = 'http://localhost:4000';
+const url = `${baseUrl}/projects`;
+
+function translateStatusToErrorMessage(status: number) {
+    switch(status) {
+        case 401:
+            return 'Please login again.';
+        case 403:
+            return 'You do not have permission to view the project(s).';
+        default:
+            return 'There was an error retrieving the project(s). Please try again';
+    }
+}
+
+function checkStatus(response: any) {
+    if(response.ok) {
+        return response;
+    } else {
+        const httpErrorInfo = {
+            status: response.status,
+            statusText: response.statusText,
+            url: response.url
+        };
+        console.log(`log server http error: ${JSON.stringify(httpErrorInfo)}`);
+
+        let errorMessage = translateStatusToErrorMessage(httpErrorInfo.status);
+        throw new Error(errorMessage);
+    }
+}
+
+function parseJson(response: Response) {
+    return response.json();
+}
+
+//eslint-disable-next-line
+function delay(ms: number) {
+    return function (x: any): Promise<any> {
+        return new Promise((resolve) => setTimeout(() => resolve(x), ms)); 
+    };
+}
+
+function convertToProjectModels(data: any[]): Project[] {
+    let projects: Project[] = data.map(convertToProjectModel);
+    return projects;
+}
+
