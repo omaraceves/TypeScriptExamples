747c4feea0a4fd7435aeb004a799c86120acdf1d
Omar.Aceves <lcc.omar.aceves@gmail.com>
2023-02-23 11:52:02 -0800

    handsonreact lab 19 put

--- Tutorials/HandsOnReact/keeptrack/src/projects/ProjectsPage.tsx
+++ Tutorials/HandsOnReact/keeptrack/src/projects/ProjectsPage.tsx
@@ -38,10 +38,19 @@ function ProjectsPage() {
     }, [currentPage])
 
     const saveProject = (project: Project) => {
-        let updatedProjects = projects.map((p: Project) => {
-            return p.id === project.id ? project: p;
+        projectApi
+        .put(project)
+        .then((updatedProject) => {
+            let updatedProjects = projects.map((p: Project) => {
+                return p.id === project.id ? new Project(updatedProject): p;
+            });
+            setProjects(updatedProjects);
+        })
+        .catch((e) => {
+            if (e instanceof Error) {
+                setError(e.message);
+            }
         });
-        setProjects(updatedProjects);
     };
 
     return (
