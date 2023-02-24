86f490b5709e67a61e11d702489f9db4dab6ea84
Omar.Aceves <lcc.omar.aceves@gmail.com>
2023-02-24 15:10:55 -0800

    comment on state

--- Tutorials/HandsOnReact/keeptrack/src/projects/ProjectsPage.tsx
+++ Tutorials/HandsOnReact/keeptrack/src/projects/ProjectsPage.tsx
@@ -41,9 +41,16 @@ function ProjectsPage() {
         projectApi
         .put(project)
         .then((updatedProject) => {
+            //iterates over projects from State
+            //for each it evaluates its id with the id of the input Project param
+            //if it matches it creates a new Project taking the input Project para as input
+            //if it doesn't match, then it takes the current project within the iteration
+            //the result is a new array where updated projects replaced older versions
             let updatedProjects = projects.map((p: Project) => {
                 return p.id === project.id ? new Project(updatedProject): p;
             });
+
+            //sets value for States with updated projects
             setProjects(updatedProjects);
         })
         .catch((e) => {
