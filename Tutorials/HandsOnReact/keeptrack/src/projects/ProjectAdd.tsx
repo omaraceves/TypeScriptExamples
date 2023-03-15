5fa24c2a4173ab74de1244cb98966cae18f9cfb7
Omar.Aceves <lcc.omar.aceves@gmail.com>
2023-03-14 23:49:11 -0700

    Add additions

--- Tutorials/HandsOnReact/keeptrack/src/projects/ProjectAdd.tsx
+++ Tutorials/HandsOnReact/keeptrack/src/projects/ProjectAdd.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Project } from './Project';
 import ProjectForm from './ProjectForm';
+import { projectApi } from './projectApi';
 
 interface ProjectAddProps {
 }
 export default function ProjectAdd() {
+
+  const saveProject = (project: Project) => {
+    projectApi
+    .add(project)
+    .catch((e) => {
+        if (e instanceof Error) {
+            console.log(e.message);
+        }
+    });
+};
+
   return (
     <div className="row">
-        <ProjectForm onCancel={() => {}} onSave={() => {}} onDelete={() => {}} project={new Project()}/>
+        <ProjectForm onCancel={() => {}} onSave={saveProject} onDelete={() => {}} project={new Project()}/>
     </div>
 );
 }
